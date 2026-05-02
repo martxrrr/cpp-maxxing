98e8ed8d8682e33b24f226bd04d477a5a88d2fa7
Martin <wainainam008@gmail.com>
2026-05-03 01:54:40 +0300

    operator overloading

---
+++ day33/operOverload.cxx
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main(){
+
+
+
+    return 0;
+}
+
+/*
+AN OPERATOR is a symbol that we use instead of a function
+e.g & - ampersand
+    || - bitwise operator
+    <<
+    >>
+    ->
+    %
+    +
+    -
+    and many more
+
+
+
+
+
+*/
