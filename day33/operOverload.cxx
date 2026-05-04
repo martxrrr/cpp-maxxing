5e4154aecfe76f260884a80a676b40d9d63d9d52
Martin <wainainam008@gmail.com>
2026-05-04 23:30:26 +0300

    assignment on opertor overloading

--- day33/operOverload.cxx
+++ day33/operOverload.cxx
@@ -1,14 +1,25 @@
 #include <iostream>
 
-class Person{
-    std::string name;
-    int age;
+class Complex{
+    int a;
+    int b;
     public:
-    Person(): name(name), age(age) {}
+    Complex(int a = 0, int b = 0) : b(b), a(a) {}
+
+    Complex operator + (const Complex& other){
+        return (a + other.a, b + other.b);
+    }
+    void display(){
+        std::cout << a << " " << b << std::endl;
+    }
 };
 
-
 int main(){
+    Complex c1(2, 3);
+    Complex c2(3, 4);
+    Complex c3 = c1 + c2;
+
+    c3.display();
 
     std::cout << "Hello World" << std::endl;
 
