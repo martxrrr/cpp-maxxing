c4413afec4fcdb3e1aef048e7aee9ba40fdf29ff
Martin <wainainam008@gmail.com>
2026-05-12 22:55:09 +0300

    calculus did not cook me

--- day35/cooked.cpp
+++ day35/cooked.cpp
@@ -11,5 +11,13 @@ int main(){
 	std::cout << text3 << std::endl;
 
 
+	//huge update
+	std::string final = "R(
+	I did not get cooked, I was so afraid of failing the CAT but that lecturer said that we should do it as a group discussion,
+	I have never felt much relieve man. At least now I have a chance to recover and study for the final exam and aim for that A 
+	haha, I love you Calculus lecturer.
+	)";
+
+
 	return 0;
 }
