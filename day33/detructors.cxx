211a93b5f3f17f63331162c246bd5cd40d8269be
martxrrr <wainainam008@gmail.com>
2026-06-11 21:06:53 +0300

    credit card

--- day33/detructors.cxx
+++ day33/detructors.cxx
@@ -19,10 +19,87 @@ void getInfo(Teacher teacher){
 	std::cout << "Age: " << teacher.age << "\n";
 }
 
+
+class Temperature {
+private:
+    double celsius;
+
+public:
+    Temperature(double c) : celsius(c) {}
+    friend void printFahrenheit(Temperature t); 
+};
+
+void printFahrenheit(Temperature t) {
+    double fahrenheit = (t.celsius * 9.0 / 5.0) + 32.0;
+    std::cout << "Temperature: " << fahrenheit << "F" << std::endl;
+}
+
+//member function of another class as a friend
+
+class Account; 
+
+class Auditor {
+public:
+    void auditAccount(const Account& acct);
+};
+
+class Account {
+private:
+    double balance;
+
+public:
+    Account(double b) : balance(b) {}
+    friend void Auditor::auditAccount(const Account& acct);
+};
+
+void Auditor::auditAccount(const Account& acct) {
+    std::cout << "Auditing Balance: $" << acct.balance << std::endl;
+}
+
+//friend classes
+class Engine {
+private:
+    int horsepower;
+    bool isRunning;
+
+public:
+    Engine(int hp) : horsepower(hp), isRunning(false) {}
+
+    friend class Dashboard; 
+};
+
+class Dashboard {
+public:
+    void ignite(Engine& e) {
+        e.isRunning = true; 
+        std::cout << "Engine ignited." << std::endl;
+    }
+
+    void showStatus(const Engine& e) {
+        std::cout << "Engine HP: " << e.horsepower 
+                  << " | Status: " << (e.isRunning ? "ON" : "OFF") << std::endl;
+    }
+};
+
+
 int main(){
 
 	Teacher t1;
 	getInfo(t1);
+
+	Temperature temp(25.0);
+    printFahrenheit(temp); 
+
+    Account myAcc(5000.50);
+    Auditor externalAuditor;
+    externalAuditor.auditAccount(myAcc);
+
+    Engine v8(450);
+    Dashboard dash;
+    
+    dash.showStatus(v8);
+    dash.ignite(v8);
+    dash.showStatus(v8);
 
 
 	return 0;
@@ -46,5 +123,17 @@ Destructors
 
 A friend function is a mechanism that grants non-member functions or other classes full access privileges to the private and protected sectors of a class.
 can be declared in the private, public or protected sections
+
+Friendship is not mutual
+Friednship is not transitive
+Frienship is not inherited
+
+It is not a member of the class, meaning it cannot be called using the object dot (.) or arrow (->) operators.
+Member friend functions:
+	Instead of making an entire class a friend, you can point to a specific function of that class to be a friend
+Friend classes:
+	You can also declare a class to be a friend of another class, 
+	all the member function of the friend class will be able to access the
+	data of that class
 
 */
