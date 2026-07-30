a0208f387890654521a8d000300fac8402938847
martxrrr <wainainam008@gmail.com>
2026-07-30 22:35:40 +0300

    Binary I/O and std::filesystem

--- day44/BinaryIO.cpp
+++ day44/BinaryIO.cpp
@@ -25,7 +25,9 @@ int main(){
 /*
  Binary I/O in C++ reads and writes raw memory bytes directly to and from a storage target without applying text transformations or formatting.
 
- reinterpret_cast is a low-level C++ cast operator that reinterprets the underlying bit pattern of an object as a different type without changing the data itself.  It primarily serves as a compile-time directive that instructs the compiler to treat an expression as if it had a different type, often bypassing standard type safety checks. 
+ reinterpret_cast is a low-level C++ cast operator that reinterprets the underlying bit pattern of an object as a different type without changing the data itself.  
+ It primarily serves as a compile-time directive that instructs the compiler to treat an expression as if it had a different type, 
+ often bypassing standard type safety checks. 
  Mostly used in Binary file I/O, network packet parsing, hardware register access.
 
 */
